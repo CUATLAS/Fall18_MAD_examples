6990bd37988f85beecfacad2348a36b3ab5b1963
Aileen Pierce <aileen.pierce@colorado.edu>
2018-11-05 20:14:13 -0700

    controls

---
+++ week 11 Java and UI Controls /Sport/app/src/main/java/com/example/aileen/sport/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.aileen.sport;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void findSport(View view) {
+        //toggle button
+        ToggleButton toggle = findViewById(R.id.toggleButton);
+        boolean location = toggle.isChecked();
+
+        //spinner
+        Spinner exercise = findViewById(R.id.spinner);
+        String exerciseType = String.valueOf(exercise.getSelectedItem());
+
+        //radio buttons
+        RadioGroup cost = findViewById(R.id.radioGroup);
+        int cost_id = cost.getCheckedRadioButtonId();
+
+        //check boxes
+        CheckBox winterCheckBox = findViewById(R.id.checkBox1);
+        Boolean winter = winterCheckBox.isChecked();
+
+        CheckBox springCheckBox = findViewById(R.id.checkBox2);
+        Boolean spring = springCheckBox.isChecked();
+
+        CheckBox summerCheckBox = findViewById(R.id.checkBox3);
+        Boolean summer = summerCheckBox.isChecked();
+
+        CheckBox fallCheckBox = findViewById(R.id.checkBox4);
+        Boolean fall = fallCheckBox.isChecked();
+
+        //pick sport
+        String perfectSport;
+
+        //check radio buttons
+        if (cost_id == -1) {
+            //toast
+            Context context = getApplicationContext();
+            CharSequence text = "Please select a cost level";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else {
+            if (location) { //inside
+                if (cost_id == R.id.radioButton1) { //cheapest option
+                    perfectSport = "A home workout";
+                } else {
+                    switch (exerciseType) {
+                        case "cardio":
+                            perfectSport = "Spin class";
+                            break;
+                        case "strength":
+                            perfectSport = "Weight training";
+                            break;
+                        case "flexibility":
+                            perfectSport = "Yoga";
+                            break;
+                        default:
+                            perfectSport = "Yoga";
+                    }
+                }
+            } else { //outside
+                if (cost_id == R.id.radioButton3) {
+                    if (winter) {
+                        perfectSport = "Skiing";
+                    } else {
+                        perfectSport = "Sky Diving";
+                    }
+                } else {
+                    switch (exerciseType) {
+                        case "cardio":
+                            perfectSport = "Running";
+                            break;
+                        case "strength":
+                            perfectSport = "Climbing";
+                            break;
+                        case "flexibility":
+                            perfectSport = "Yoga";
+                            break;
+                        default:
+                            perfectSport = "Yoga";
+                    }
+                }
+            }
+
+            //text view
+            TextView sportSelection = findViewById(R.id.sportTextView);
+            sportSelection.setText(perfectSport + " is the sport for you");
+        }
+    }
+}
